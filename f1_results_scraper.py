19b8d2d2060e081e2a20298eaff6d2e453a0ad91
Aryan Khanna <148300045+aryank09@users.noreply.github.com>
2024-12-13 15:47:53 -0800

    scraper (method version)

--- f1_results_scraper.py
+++ f1_results_scraper.py
@@ -1,28 +1,39 @@
 import requests
 from bs4 import BeautifulSoup
 
-url_practice_1="https://www.formula1.com/en/results/2024/races/1252/abu-dhabi/practice/1"
+def result_scraper(url):
+    #url="https://www.formula1.com/en/results/2024/races/1252/abu-dhabi/practice/1"
 
-response = requests.get(url_practice_1)
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, 'html.parser')
-    rows = soup.select("tr")
-else:
-    print("Failed to retrieve the webpage.")
-    exit()
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        rows = soup.select("tr")
+    else:
+        print("Failed to retrieve the webpage.")
+        exit()
 
-for row in rows:
-        #for all td tag elements
-        cells = row.find_all("td")
-        
-        if len(cells) > 0:
-            #getting the specific required data points 
-            driver = cells[2].get_text(strip=True)
-            team = cells[3].get_text(strip=True)
-            lap_time = cells[4].get_text(strip=True)
+    result = []
 
-            # Print the extracted data
-            print(f"Driver: {driver}, Team: {team}, Lap Time: {lap_time}")
+    for row in rows:
+            #for all td tag elements
+            cells = row.find_all("td")
+            
+            if len(cells) > 0:
+                #getting the specific required data points 
+                driver = cells[2].get_text(strip=True)
+                team = cells[3].get_text(strip=True)
+                lap_time = cells[4].get_text(strip=True)
 
-else:
-    print(f"Failed to fetch the webpage. Status code: {response.status_code}")
+                # Print the extracted data
+                #print(f"Driver: {driver}, Team: {team}, Lap Time: {lap_time}")
+                #inserting all the required data into an array
+                temp = []
+                temp.append(driver)
+                temp.append(team)
+                temp.append(lap_time)
+                result.append(temp)
+            else:
+                print(f"Failed to fetch the webpage. Status code: {response.status_code}")
+                return    
+    return result
+    
