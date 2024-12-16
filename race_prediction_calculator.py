05800dc2c62adee77601e81bfbaa268e6bb92bc8
Aryan Khanna <148300045+aryank09@users.noreply.github.com>
2024-12-15 23:21:10 -0800

    getting all weekend results and creating TODO

--- race_prediction_calculator.py
+++ race_prediction_calculator.py
@@ -1,5 +1,12 @@
 import f1_results_scraper as scrpr
 
-results = scrpr.result_scraper("https://www.formula1.com/en/results/2024/races/1252/abu-dhabi/practice/1")
+results1 = scrpr.result_scraper("https://www.formula1.com/en/results/2024/races/1252/abu-dhabi/practice/1")
+results2 = scrpr.result_scraper("https://www.formula1.com/en/results/2024/races/1252/abu-dhabi/practice/2")
+results3 = scrpr.result_scraper("https://www.formula1.com/en/results/2024/races/1252/abu-dhabi/practice/3")
 
-print(results)
+#TODO: 
+#format example ['LandoNorrisNOR', 'McLaren Mercedes', '1:24.542']
+#keep in mind time is str
+#keep in mind since practice session other drivers may be present than racing
+#so we have to add their time and divide it by the times they were in the practice session instead of 3
+print(results1)
