98fef65c53de7f3dff6f84fe4aa66ba4a15b5e3a
Aryan Khanna <148300045+aryank09@users.noreply.github.com>
2024-12-18 10:57:35 -0800

    created driver file; adjusteed calculator to take weekend name and build url

---
+++ race_prediction_driver.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    print("Welcome to the F1 Results URL Generator!")
+    
+    # Get user input for the race name
+    race_name = input("Enter the race name: ").strip()
--- temp_race_prediction_calculator.py
+++ temp_race_prediction_calculator.py
@@ -5,6 +5,32 @@ import f1_results_scraper as scrpr
 #keep in mind since practice session other drivers may be present than racing
 #so we have to add their time and divide it by the times they were in the practice session instead of 3
 
+race_id_mapping = {
+    "bahrain": 1229,
+    "saudi-arabia": 1230,
+    "australia": 1231,
+    "azerbaijan": 1232,
+    "miami": 1233,
+    "emilia-romagna": 1234,
+    "monaco": 1235,
+    "spain": 1236,
+    "canada": 1237,
+    "austria": 1238,
+    "britain": 1239,
+    "hungary": 1240,
+    "belgium": 1241,
+    "netherlands": 1242,
+    "italy": 1243,
+    "singapore": 1244,
+    "japan": 1245,
+    "qatar": 1246,
+    "united-states": 1247,
+    "mexico": 1248,
+    "brazil": 1249,
+    "las-vegas": 1250,
+    "abu-dhabi": 1252
+}
+
 def find_string_index(target):
     for outer_index, inner_list in enumerate(total_results):
         if target in inner_list:
@@ -40,34 +66,49 @@ def predictor(total_results):
         prediction.append(temp)
     return prediction
 
+def build_f1_url(race_name, session_number):
+    race_name_key = race_name.lower().replace(" ", "-")
+    
+    #Get race ID from the dictionary
+    race_id = race_id_mapping.get(race_name_key)
+    
+    if not race_id:
+        return None 
+
+    #Construct and return the URL
+    return f"https://www.formula1.com/en/results/2024/races/{race_id}/{race_name_key}/{session}/{session_number}"
+
 results1 = scrpr.result_scraper("https://www.formula1.com/en/results/2024/races/1252/abu-dhabi/practice/1")
 results2 = scrpr.result_scraper("https://www.formula1.com/en/results/2024/races/1252/abu-dhabi/practice/2")
 results3 = scrpr.result_scraper("https://www.formula1.com/en/results/2024/races/1252/abu-dhabi/practice/3")
 results4 = scrpr.result_scraper("https://www.formula1.com/en/results/2024/races/1252/abu-dhabi/starting-grid")
 
-total_results = []
+def prediction_driver(race_weekend_name):
 
-driver_name = set()
+    #TODO: make the code take results generated in the below loop and make it build the url
+    total_results = []
 
-for x in range(1,5):
-    results = eval(f"results{x}") if f"results{x}" in globals() else []
-    for i in range(len(results)):
-        if results[i][0] not in driver_name:
-            total_seconds = time_in_seconds(results[i][2])
-            results[i][2] = total_seconds
-            count = 1
-            results[i].append(count)
-            total_results.append(results[i])
-            driver_name.add(results[i][0]) 
-        else:
-            outer_index = find_string_index(results[i][0])
-            if outer_index is not None:
+    driver_name = set()
+
+    for x in range(1,5):
+        results = eval(f"results{x}") if f"results{x}" in globals() else []
+        for i in range(len(results)):
+            if results[i][0] not in driver_name:
                 total_seconds = time_in_seconds(results[i][2])
-                total_results[outer_index][2] += total_seconds
-                total_results[outer_index][3] += 1
+                results[i][2] = total_seconds
+                count = 1
+                results[i].append(count)
+                total_results.append(results[i])
+                driver_name.add(results[i][0]) 
+            else:
+                outer_index = find_string_index(results[i][0])
+                if outer_index is not None:
+                    total_seconds = time_in_seconds(results[i][2])
+                    total_results[outer_index][2] += total_seconds
+                    total_results[outer_index][3] += 1
 
-prediction = predictor(total_results)
-sorted_prediction = sorted(prediction, key=lambda x: x[2])
-formated_prediction = format_prediction(sorted_prediction)
-for index, result in enumerate(formated_prediction, start=1):
-    print(f"{index}. {result}")
+    prediction = predictor(total_results)
+    sorted_prediction = sorted(prediction, key=lambda x: x[2])
+    formated_prediction = format_prediction(sorted_prediction)
+    for index, result in enumerate(formated_prediction, start=1):
+        print(f"{index}. {result}")
