7483ab3b4b8ebd8eb3c16cdf33c0ca2596b888bb
Aryan Khanna <148300045+aryank09@users.noreply.github.com>
2024-12-18 23:27:56 -0800

    small change to print statement for driver

--- race_prediction_driver.py
+++ race_prediction_driver.py
@@ -1,9 +1,9 @@
 import temp_race_prediction_calculator as prdctr
 
 if __name__ == "__main__":
-    print("Welcome to the F1 Results URL Generator!")
+    print("Welcome to the F1 Race Results predictor!")
     
-    # Get user input for the race name
+    #Get user input for the race name
     race_name = input("Enter the race name: ").strip()
 
     prdctr.prediction_driver(race_name)
